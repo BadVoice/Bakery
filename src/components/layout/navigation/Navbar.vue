856c2aed2c1c220495123a27f2b44b29dfcbe36c
Herman <smurovnemez@mail.ru>
2022-09-20 06:28:34 +0300

    feat: adaptive navbar & condtition render search inpt

--- src/components/layout/navigation/Navbar.vue
+++ src/components/layout/navigation/Navbar.vue
@@ -22,7 +22,8 @@
             <!-- mobile toggle menu-->
             <div class="mobile__menu">
                     
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1
+                " stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
                 
@@ -35,16 +36,21 @@
 
 
             <div class="nav__interaction-user">
-                <input v-model="inp"
+                <input v-if="isOpen"
                     class=" interaction-user__search-input" type="text">
-                {{ inp }}
-                <img class="interaction-user__search-img" src="./icons/searchIcon.svg" alt="seacrhIcon">
+                
+                <a 
+                    @click="isOpen = !isOpen"
+                >
+                    <img 
+                    class="interaction-user__search-img" src="./icons/searchIcon.svg" alt="seacrhIcon">
+                </a>
                 
 
                 <div class="nav_interaction-profile">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.7" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-                    </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.7" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
                     
                 </div>
 
@@ -65,7 +71,7 @@
 <script setup>
     import { ref, } from "@vue/reactivity";
     
-
+    const isOpen = ref()
 
 </script>
 

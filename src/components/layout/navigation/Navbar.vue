bb673dcbb3300e88984a56682479b6e27b3cb550
Herman <smurovnemez@mail.ru>
2022-10-02 01:45:27 +0300

    feat: add Hamburger Menu

--- src/components/layout/navigation/Navbar.vue
+++ src/components/layout/navigation/Navbar.vue
@@ -22,12 +22,13 @@
                 </div>
     
                 <!-- mobile toggle menu-->
-                <div class="mobile__menu">
+                <div v-bind:class="{ active: isActive }"
+                    @click="isActive = !isActive"
+                    class="toggle ">
                         
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1
-                    " stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 
                 </div>
     
@@ -176,6 +177,7 @@
     
     const isOpen = ref()
     const openMenu = ref(false)
+    const isActive = ref(true)
 
 </script>
 

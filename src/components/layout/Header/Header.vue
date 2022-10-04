7f9379c5bfc8a73054a0cbb2d02f3b0cb7f306b9
Herman <smurovnemez@mail.ru>
2022-10-04 22:45:41 +0300

    feat: add header content

--- src/components/layout/Header/Header.vue
+++ src/components/layout/Header/Header.vue
@@ -1,14 +1,17 @@
 <template>
     <header class="header">
+        <div class="header-overlay"> </div>
         <div class="container">
-            <h1></h1>
-            <h5></h5>
+            <div class="header-flex">
+                <h1 class="header__title">Happy New Year</h1>
+            <h5 class="header__subtitle">Discover Our Festive  Range</h5>
 
             <div>
-                <button>
-                    <p></p>
-                    <img src="" alt="">
+                <button class="header-btn">
+                    <p class="header-btn__descr">Shop Now</p>
+                    <img class="header-btn__icon" src="./icons/down-arrow.png" alt="arrow-icon">
                 </button>
+            </div>
             </div>
 
         </div>
